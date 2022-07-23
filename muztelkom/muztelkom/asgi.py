a859bee6b8aad66272bc762cce8ed7abb52ee2e4
Dawid Zareba <dawid.zareba03@gmail.com>
2022-07-23 13:24:56 +0200

    Optimalize requirements

--- muztelkom/muztelkom/asgi.py
+++ muztelkom/muztelkom/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for MuzTelKom project.
+ASGI config for muztelkom project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MuzTelKom.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'muztelkom.settings')
 
 application = get_asgi_application()
--- muztelkom/muztelkom/wsgi.py
+++ muztelkom/muztelkom/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for MuzTelKom project.
+WSGI config for muztelkom project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MuzTelKom.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'muztelkom.settings')
 
 application = get_wsgi_application()
